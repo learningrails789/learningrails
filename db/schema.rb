0ee5a72789ce42903f4e1b5929495a3549fbb9fe
foo <learningrails@mail.com>
2017-04-26 15:32:15 +0000

    skils model added but nothing saved to the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421210239) do
+ActiveRecord::Schema.define(version: 20170426151615) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "percen_utilized"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
