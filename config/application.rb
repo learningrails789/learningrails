2499bf7951323de880300cf23c87b3228cedf6fd
foo <learningrails@mail.com>
2017-05-05 15:39:06 +0000

    learned strong params and finally added slug based routes

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Newapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_controller.permit_all_paramters = true
   end
 end
